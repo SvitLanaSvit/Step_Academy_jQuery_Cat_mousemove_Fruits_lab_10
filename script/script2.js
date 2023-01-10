0a35c567d6013d393c6f83a4611287efd6f9efaa
Spil_jf00@student.itstep.org <Spil_jf00@student.itstep.org>
2023-01-10 22:01:41 +0100

    changing

--- script/script2.js
+++ script/script2.js
@@ -1,26 +1,9 @@
-//-------1-------
-$(()=>{
-    $("#main").bind("mousemove", function(event){
-        $("#image").css({"left" : event.pageX, "top" : event.pageY});
-    })
-});
-
 //-------2-------
 $(()=>{
     $(".button").bind("mouseenter", function(){
-        $(this).css('box-shadow', '0px 0px 8px 3px #000000');
-    });
-});
-
-$(()=>{
-    $(".button").bind("mouseout", function(){
-        $(this).css('box-shadow', '0px 0px 8px 3px #FFFFFF');
-    })
-});
-
-$(()=>{
-    $('.button').bind('click', function(){
         var color = $(this).css('background-color');
+        console.log(color);
+        $(this).css('box-shadow', '0px 0px 8px 3px #000000');
 
         var list = $(".conteinerFruits").children();
         list.each((index,elem) =>{
@@ -28,10 +11,20 @@ $(()=>{
             if($(elem).css('background-color') == color){
                 $(elem).css('box-shadow', '0px 0px 8px 3px #000000');
             }
+        });
+    });
+});
 
-            if($(elem).css('background-color') != color){
+$(()=>{
+    $(".button").bind("mouseout", function(){
+        var color = $(this).css('background-color');
+        $(this).css('box-shadow', '0px 0px 8px 3px #FFFFFF');
+
+        var list = $(".conteinerFruits").children();
+        list.each((index,elem) =>{
+            if($(elem).css('background-color') == color){
                 $(elem).css('box-shadow', '0px 0px 8px 3px #FFFFFF');
             }
         });
-    });
+    })
 });
